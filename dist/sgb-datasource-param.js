4d51425f8e21ca622377ca9891499f6968307dad
Saul Hernandez <sauldhernandez@gmail.com>
2015-03-30 15:46:13 -0430

    renamed factory

--- dist/sgb-datasource-param.js
+++ dist/sgb-datasource-param.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('sgb-datasource-param', ['megazord'])
-    .factory('_paramDatasource', [function (){
+    .factory('sgb-datasource-param', [function (){
         return {
             getData: function(params) {
                 //This is simply a pass-through, as transform can handle the plucking out of this data object.
